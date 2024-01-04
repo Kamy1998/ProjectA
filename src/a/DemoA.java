c3e6663b30570cfea9840455364bb0a5b229ba4f
Kamy <Kamy@4Ever.mynetworksettings.com>
2024-01-03 21:14:48 -0500

    Initial Commit

---
+++ src/a/DemoA.java
@@ -0,0 +1,13 @@
+package a;
+
+public class DemoA {
+	
+public static void main(String[] args) {
+		
+		System.out.println("Monday");
+		System.out.println("ABC");
+		System.out.println("DCE");
+		
+	}
+
+}
